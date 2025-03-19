ff9835a3bcb4b73cde3354508b465023d5a9ed6d
Feyli <mathiscom64@gmail.com>
2025-03-19 14:30:00 +0100

    refactor: streamline grid setup in BaseScene by consolidating image creation and adjusting origin

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -7,9 +7,6 @@ export default class BaseScene extends Phaser.Scene {
 
     createShared() {
         this.add.image(0, 0, 'floor').setScale(1).setDisplayOrigin(0, 0);
-
-        const grid = this.add.image(0, 0, 'quadrillage').setScale(1);
-        grid.x = grid.displayWidth / 2;
-        grid.y = grid.displayHeight / 2;
+        this.add.image(0, 0, 'quadrillage').setScale(1).setOrigin(0, 0);
     }
 }
